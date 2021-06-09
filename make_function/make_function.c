89b3bf79ae7e3c0118fa37dab2e5a5e1fa752b02
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-06-09 17:46:23 +0900

    2.9 함수 만들기

---
+++ make_function/make_function.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+// prototyping 또는 function declaration
+void say_hello_two();
+
+// return type이 없기 때문에 void
+void say_hello(void) {
+	
+	printf("Hello, World!\n");
+
+	return; // return type이 void면 return 생략가능
+}
+
+int main() {
+
+	say_hello();
+	say_hello_two();
+
+	return 0;
+}
+
+// function definition
+void say_hello_two() {
+
+	printf("Hello, World!\n");
+	printf("Hello, World!\n");
+}
