e01e176b950e3f59099a69cc312bfdd8fd851174
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-06-25 18:58:30 +0900

    3.3 scanf() 함수의 기본적인 사용법

---
+++ basic_usage_of_scanf_function/basic_usage_of_scanf_function.c
@@ -0,0 +1,19 @@
+﻿// scanf() 에러 해결법 1
+// #define _CRT_SECURE_NO_WARNINGS
+
+// scanf() 에러 해결법 2 
+// project 우클릭, properties, c/c++. preprecessor에 _CRT_SECURE_NO_WARNINGS 추가
+
+#include <stdio.h>
+
+int main() {
+	int i = 0;
+
+	// scan formated
+	scanf("%d", &i); // & : ampersand
+
+	// print formated
+	printf("Value is %d\n", i);
+
+	return 0;
+}
