5adda8db150a46cf23a6b29c8e0f40c4062c2154
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-06-29 16:30:15 +0900

    3.13 불리언형

---
+++ boolean_types/boolean_types.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+	printf("%u\n", sizeof(_Bool)); // 1 byte (자료형이 가질 수 있는 최소 메모리)
+
+	_Bool b1;
+	b1 = 0; // false
+	printf("%d\n", b1);
+	b1 = 1; // true
+	printf("%d\n", b1);
+
+	bool b2, b3;
+	b2 = true;
+	b3 = false;
+
+	printf("%d %d\n", b2, b3);
+
+
+	return 0;
+}
