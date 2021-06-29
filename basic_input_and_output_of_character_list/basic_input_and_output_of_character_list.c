cf047cc6e731f75cae3dc9b376c0000254351fa5
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-06-29 16:54:37 +0900

    4.1 문자열 입출력하기

---
+++ basic_input_and_output_of_character_list/basic_input_and_output_of_character_list.c
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	// 1 byte 짜리 40개 확보(배열) -> 대표하는 메모리 주소 1개만 
+	char fruit_name[40]; // stores only on character.
+
+	printf("What is your favorite fruit?\n");
+
+	// %s : string / 배열을 입력받을 땐 & 없어짐(중요!)
+	scanf("%s", fruit_name); // be careful with &
+
+	printf("You like %s!\n", fruit_name);
+
+	return 0;
+}
