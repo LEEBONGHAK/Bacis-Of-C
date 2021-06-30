b72eb39a93d1094f0f984f5c723bf0e817cd2751
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-06-30 20:37:25 +0900

    5.1 반복 루프와의 첫 만남

---
+++ first_meeting_with_loop/first_meeting_with_loop.c
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	int n = 1, i = 1;
+	int j;
+
+	// 전통적인 방법(거의 사용하지 않음)
+label:
+	printf("%d\n", n);
+	n = n + 1;
+
+	if (n == 10) goto out;
+
+	goto label;
+
+out:
+
+	// loop 1
+	while (i < 10) {
+		printf("%d\n", i);
+		i = i + 1;
+	}
+
+	// loop 2
+	for (j = 1; j < 10; j++) {
+		printf("%d\n", j);
+	}
+
+	return 0;
+}
