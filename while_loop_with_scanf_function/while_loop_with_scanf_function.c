957e8cb542b0b6aa0d100f97b1386a583177fb84
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-02 00:09:53 +0900

    6.1 while 반복 루프에서 scanf()의 반환값 사용하기

---
+++ while_loop_with_scanf_function/while_loop_with_scanf_function.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	int num, sum = 0;
+	int status;
+	
+	printf("Enter an integer (q to quit) : ");
+	status = scanf("%d", &num);			// return value of scanf() => 입력 받은 갯수
+
+	while (status == 1) {				// equality operator
+		sum += num;
+
+		printf("Enter next integer (q to quit) : ");
+		status = scanf("%d", &num);
+	}
+
+	printf("Sum = %d\n", sum);
+
+
+	// TODO : C-style shortcut
+	printf("Enter an integer (q to quit) : ");
+
+	while (scanf("%d", &num) == 1) {				// equality operator
+		sum += num;
+
+		printf("Enter next integer (q to quit) : ");
+	}
+
+	printf("Sum = %d\n", sum);
+
+	return 0;
+}
