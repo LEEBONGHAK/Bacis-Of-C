2422fd759a189972cdaba99cf8b0fc7c8c1393dc
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-03 00:59:26 +0900

    6.13 탈출조건 루프 do while

---
+++ exit-condition_loop_do_while/exit-condition_loop_do_while.c
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	const int secret_code = 337;
+	
+	int guess = 0;
+
+	printf("Enter secret code : ");
+	scanf("%d", &guess);
+
+	while (guess != secret_code) {
+		printf("Enter secret code : ");
+		scanf("%d", &guess);
+	}
+
+	printf("Good!\n");
+
+
+	// do while문 사용하기 - 비교를 하기전에 무조건 1 번은 실행함
+	do {
+		printf("Enter secret code : ");
+		scanf("%d", &guess);
+	} 
+	while (guess != secret_code);
+
+	printf("Good!\n");
+
+	return 0;
+}
