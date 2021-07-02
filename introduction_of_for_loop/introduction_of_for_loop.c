36481939c1a5931bad51e29bcc277b5e02ff5b0c
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-02 21:42:19 +0900

    6.8 for 루프 소개

---
+++ introduction_of_for_loop/introduction_of_for_loop.c
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	int i = 1;
+
+	// Indefinite loop (while) vs. Counting loop (for)
+	while (i <= 10) {
+		printf("%d\n", i);
+		i++;
+	}
+
+
+	/*
+		Counting Loop
+		1. Counter initialization
+		2. Counter check
+		3. Counter change
+	*/
+
+	for (i = 1; i <= 10; i++) {
+		printf("%d\n", i);
+	}
+
+	return 0;
+}
