31a926bc1a749500234b4db8514b89a211ace806
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-02 21:25:57 +0900

    6.7 관계 연산자의 우선순위

---
+++ precedence_of_relational_operators/precedence_of_relational_operators.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	int x = 1, y = 2, z;
+
+	// ()을 통해 우선 순위를 알려주는 것 
+	// 우선 순위가 같으면 왼쪽에서 오른쪽으로
+	x > y + 2;
+	x > (y + 2);
+
+	x = y > 2;
+	x = (y > 2);
+
+	z = x > y;
+	z = (x > y);
+
+	x != y == z;
+	(x != y) == z;
+
+	// ref : Google 'Operators Precedence in C'
+
+	return 0;
+}
