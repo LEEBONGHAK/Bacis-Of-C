0b30475f9649b4b130264e6636d2e355219f1eb1
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-02 22:36:25 +0900

    6.10 다양한 대입 연산자들 (그리고 어셈블리 코드 확인법)

---
+++ variable_assignment_operators/variable_assignment_operators.c
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	/*
+		+=, -=, *=, /=, %=
+	*/
+
+	int i = 1024;
+	
+	i = i + 10;
+	i += 10;
+
+	i = i - 10;
+	i -= 10;
+
+	i = i * 10;
+	i *= 10;
+
+	i = i / 10;
+	i /= 10;
+
+	i = i % 4;
+	i %= 4;
+
+	i = i * (1 + 2);
+	i *= 1 + 2;
+	i *= (1 + 2); // Check precedence -> assignment operator의 precedence은 매우 낮음
+
+	for (int i = 0; i < 10; i += 2) {
+		;
+	}
+
+
+	/*
+		어셈블리 코드 확인법
+
+		1. 보고 싶은 곳 break point 지정
+		2. debuger 실행
+		3. 우클릭 후 go to disassembly or debug - windows - disassembly
+	*/
+
+	return 0;
+}
