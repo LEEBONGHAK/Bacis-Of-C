8c88185603322a1062bd9e7c57bcc1f7f756b1ac
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-04 01:09:12 +0900

    7.3 ctype.h 문자 함수들

---
+++ ctype.h/ctype.h.c
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <ctype.h>		// isalpha() and more
+
+int main() {
+	char ch;
+
+	// 대문자 <-> 소문자 변환 예제
+	while ((ch = getchar()) != '\n') {
+		//if (ch >= 'a' && ch <= 'z') {		// 'A' = 65, 'a' = 97
+		//	ch -= 'a' - 'A';
+		//}
+		//else if (ch >= 'A' && ch <= 'Z') {
+		//	ch += 'a' - 'A';
+		//}
+
+		if (islower(ch)) {
+			ch = toupper(ch);
+		}
+		else if (isupper(ch)) {
+			ch = tolower(ch);
+		}
+
+		putchar(ch);
+	}
+	printf("\n\n");
+
+
+	// 숫자 -> * 변환 예제
+	while ((ch = getchar()) != '\n') {
+		if (isdigit(ch)) {
+			ch = '*';
+		}
+
+		putchar(ch);
+	}
+	printf("\n\n");
+
+	// ref : tutorialpoint에서 ctype.h 검색
+
+	return 0;
+}
