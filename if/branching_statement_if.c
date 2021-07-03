a2b7fbe498e1ba5c54dea3c762c36942f34fd300
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-04 00:22:55 +0900

    7.1 분기문 if

---
+++ if/branching_statement_if.c
@@ -0,0 +1,46 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	/*
+		if (expression) {
+			statement
+		}
+	*/
+	int number;
+
+	printf("Inpute a positive integer : ");
+	scanf("%d", &number);
+
+	if (number % 2 == 0) {
+		printf("Even\n");
+	}
+
+	if (number % 2 != 0) {
+		printf("Odd\n");
+	}
+
+
+	/*
+		if (expression) {
+			statement1
+		}
+		else {
+			statement2
+		}
+	*/
+	int number2;
+
+	printf("Inpute a positive integer : ");
+	scanf("%d", &number2);
+
+	if (number2 % 2 == 0) {
+		printf("Even\n");
+	}
+	else {
+		printf("Odd\n");
+	}
+
+	return 0;
+}
