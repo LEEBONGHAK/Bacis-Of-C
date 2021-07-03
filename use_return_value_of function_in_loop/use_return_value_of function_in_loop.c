13659e991c676da7aaf6f802847a9c8140d8752f
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-04 00:11:18 +0900

    6.18 루프 안에서 함수의 반환값 사용하기

---
+++ use_return_value_of function_in_loop/use_return_value_of function_in_loop.c
@@ -0,0 +1,33 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int compute_pow(int base, int exp);		// prototype
+
+int main() {
+	// 3 ^ 4
+	// 3 * 3 * 3 * 3 = 81
+	int base, exp, i, result;
+	
+	while (scanf("%d %d", &base, &exp) == 2) {
+		//result = 1;
+		//for (i = 0; i < exp; ++i) {
+		//	result *= base;
+		//}
+		
+		result = compute_pow(base, exp);
+
+		printf("Result = %d\n", result);
+	}
+
+	return 0;
+}
+
+int compute_pow(int base, int exp) {
+	int i, result = 1;
+	for (i = 0; i < exp; ++i) {
+		result *= base;
+	}
+
+	return result;
+}
