7369c5938f8e7b93f7df7900cd22b99c5c99e8ed
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-03 21:08:44 +0900

    6.15 중첩된 루프들

---
+++ nested_loops/nested_loops.c
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#define NUM_ROWS	5
+#define FIRST_CHAR	'A'
+#define LAST_CHAR	'K'
+
+int main() {
+	int r;	// row loop
+	int c;	// character loop
+
+	// 예제 1
+	for (r = 0; r < NUM_ROWS; ++r) {					// outer row loop
+		for (c = FIRST_CHAR; c <= LAST_CHAR; ++c) {		// inner character loop
+			printf("%c ", c);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+
+	// 예제 2
+	for (r = 0; r < 10; ++r) {
+		for (c = FIRST_CHAR;c <= FIRST_CHAR + r; ++c) {
+			printf("%c ", c);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+
+	// 예제 3
+	for (r = 0; r < 12; ++r) {
+		for (c = FIRST_CHAR + r;c <= FIRST_CHAR + 12; ++c) {
+			printf("%c ", c);
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}
