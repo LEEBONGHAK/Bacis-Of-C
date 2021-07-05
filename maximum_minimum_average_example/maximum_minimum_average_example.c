0524037dbedba4e8c382183c45a54dbed2f1873a
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-05 17:38:32 +0900

    7.11 최대, 최소, 평균 구하기 예제

---
+++ maximum_minimum_average_example/maximum_minimum_average_example.c
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <float.h>
+
+int main() {
+	float max = -FLT_MAX;
+	float min = FLT_MAX;
+	float sum = 0.0f;
+	float input;
+	int n = 0;
+
+
+	while (scanf("%f", &input) == 1) {
+		// TODO : ignore negative values
+		if (input < 0.0f) {
+			printf("Please input positive number\n");
+			continue;
+		}
+		
+		//if (input > max) {
+		//	max = input;
+		//}
+		max = (input > max) ? input : max;
+
+		//if (input < min) {
+		//	min = input;
+		//}
+		max = (input < min) ? input : min;
+
+		sum += input;
+		n++;	// Count number of inputs
+	}
+
+	if (n > 0) {
+		printf("min = %f, max = %f, avg = %f\n", min, max, sum / n);
+	}
+	else {
+		printf("No input\n");
+	}
+
+	return 0;
+}
