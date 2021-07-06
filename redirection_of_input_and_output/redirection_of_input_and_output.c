bef9a9a0f5846aeaf0a8710e30eebe554eb9f88c
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-06 17:17:34 +0900

    8.3 입출력 방향 재지정

---
+++ redirection_of_input_and_output/redirection_of_input_and_output.c
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+// redirection_of_input_and_output.exe > output.txt : 출력 방향을 콘솔창에서에 output.txt로 재지정(redirection)
+// redirection_of_input_and_output.exe < input.txt : input.txt 내용을 받아 콘솔창에서 출력
+// redirection_of_input_and_output.exe >> input.txt : 기존 내용을 바꾸지 않고 다음 줄에 출력
+// test.exe | redirection_of_input_and_output.exe : pipe 기능
+
+int main() {
+	printf("I love apple.\n");
+
+	char str[100];
+	scanf("%s", str);
+	printf("I love %s\n", str);
+
+	return 0;
+}
