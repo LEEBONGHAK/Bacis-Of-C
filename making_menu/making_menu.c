a5778dcb97bb5ad75b27e1587229d0d06bf5e160
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-06 22:53:05 +0900

    8.9 텍스트 파일 읽기

--- making_menu/making_menu.c
+++ making_menu/making_menu.c
@@ -1,7 +1,7 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>		// exit()
 
 char get_choice(void);
 char get_first_char(void);
---
+++ read_text_file/read_text_file.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>		// exit()
+
+int main() {
+	int c;
+	FILE *file = NULL;		// 포인터 변수이며, 주소를 저장
+	char file_name[] = "my_file.txt";
+
+	file = fopen(file_name, "r");		// read or write
+	if (file == NULL) {
+		printf("Failed to open file.\n");
+		exit(1);
+	}
+	
+	while ((c = getc(file)) != EOF) {	// file의 문자를 EOF를 만나기 전까지 출력
+		putchar(c);
+	}
+	fclose(file);		// 파일 닫기
+	printf("\n");
+
+	return 0;
+}
