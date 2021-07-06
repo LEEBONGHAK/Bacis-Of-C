b0593da567f44130d6c525c4378534e191c3c90d
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-06 16:33:18 +0900

    8.1 입출력 버퍼

---
+++ input_output_buffer/input_output_buffer.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <conio.h>		// Windows에서만 작동, 버퍼를 사용하지 않는 함수 => _getch(), _getche()
+
+int main() {
+	char c;
+
+	//while ((c = getchar()) != '.') {	// 버퍼 사용
+	while ((c=_getche()) != '.') {		// 입력할 때마다 바로 출력, e = echo
+		putchar(c);
+	}
+
+	while ((c = _getch()) != '.') {		// echo가 없음
+		putchar(c);
+	}
+	printf("\n");
+
+	return 0;
+}
