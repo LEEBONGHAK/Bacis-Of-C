1d03f332864b0e67d940f69d02d9f439bd9fd23b
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-07 19:39:36 +0900

    9.4 변수의 영역과 지역 변수

---
+++ local_variables_and_area_of_variables/local_variables_and_scope_of_variables.c
@@ -0,0 +1,42 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+//int a;	// 파일 전체에서 접근이 가능
+
+int int_max(int i, int j);
+
+int int_max(int i, int j) {
+	//a = 456;
+	int m;			// stack frame
+	m = i > j ? i : j;
+	
+	return m;
+}
+
+// 변수가 유효한 영역이 존재한다.
+int main() {
+	//m = 123;		// error
+	
+	int a;		
+
+	a = int_max(1, 2);
+	printf("%d\n", a);
+	printf("%d\n", &a);		// 변수의 주소 출력
+
+	{
+		//int a;
+		a = int_max(4, 5);
+		printf("%d\n", a);
+		printf("%d\n", &a);
+
+		int b = 123;
+	}
+
+	//b = 456;
+
+	printf("%d\n", a);
+	printf("%d\n", &a);
+
+	return 0;
+}
