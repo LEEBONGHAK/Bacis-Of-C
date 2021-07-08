9321454f799aa58106cb5c58a03a419229059530
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-08 16:03:07 +0900

    9.11 헤더 파일 만드는 방법

---
+++ make_header_file/main.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>		// 함수의 정의가 만든 헤더 파일에 포함되어 있다면 없어도 됨
+#include "my_print_functions.h"
+
+int main() {
+	print_hello();
+	print_hi();
+	print_hello();
+	print_hi();
+	print_str("No one lives forever.");
+	print_str("Valar morghulis");
+
+	printf("Hello\n");
+
+	return 0;
+}
+
+/*
+	헤더 파일(header file) 만들기
+	
+	1. 일반적으로 main()이 존재하는 파일명이 main.c로 지정되어 있음
+	2. 프로젝트에서 우클릭-add-new item-header 파일 추가
+	3. 같은 이름의 c 파일을 만들어 함수의 정의를 하고, 헤더 파일에 선언만 남겨 목차처럼 사용하는 편
+*/
---
+++ make_header_file/my_print_functions.h
@@ -0,0 +1,9 @@
+#pragma once // header guard - visual studio에서 사용하는 표준임
+
+#include <stdio.h>
+
+void print_hello();
+void print_hi();
+void print_str(char* str);
+
+// assume that we have many more longer functions
