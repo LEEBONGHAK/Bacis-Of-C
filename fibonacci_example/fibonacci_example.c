d359865d55d565270951c2ad413a3715632c21a1
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-08 15:11:06 +0900

    9.10 피보나치 예제와 재귀 호출의 장단점

---
+++ fibonacci_example/fibonacci_example.c
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+/*
+	Fibonacci sequence
+	1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144
+
+	ex) fibonacci(5) = 3 + 2 = fibonacci(4) + fibonacci(3)
+	ex) fibonacci(6) = 5 + 3 = fibonacci(5) + fibonacci(4)
+*/
+
+int fibonacci(int number);
+
+int main() {
+	for (int count = 1; count < 13; ++count) {
+		printf("%d ", fibonacci(count));
+	}
+	printf("\n");
+
+	return 0;
+}
+
+int fibonacci(int number) {
+	// 내 코드
+	/*if (number == 1 || number == 2) {
+		return 1;
+	}
+
+	int i = fibonacci(number - 1) + fibonacci(number - 2);
+
+	return i;*/
+
+
+	// 강의 코드
+	if (number > 2) {
+		return fibonacci(number - 1) + fibonacci(number - 2);	// double recursion
+	}
+	else {
+		return 1;
+	}
+}
+
+/*
+	disadvantages of recursion
+
+	1. memory (in)efficiency (메모리를 많이 씀)
+	2. redundant computation (계산의 중복 발생)
+*/
