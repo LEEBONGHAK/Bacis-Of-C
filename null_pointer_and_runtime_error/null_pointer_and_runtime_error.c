d7d461d7c3d8a21d9c59b098f698088bbe6b4f67
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-09 17:16:22 +0900

    9.15 NULL 포인터와 런타임 에러

---
+++ null_pointer_and_runtime_error/null_pointer_and_runtime_error.c
@@ -0,0 +1,39 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	int a;		// 초기화하지 않으면 쓰레기 값이 들어가 있음
+
+	printf("%p\n", &a);
+	printf("%d\n", a);
+	
+	
+	/*int* ptr;	// 쓰레기 값이 주소가 되지 않기 때문에 error 발생
+
+	printf("%p\n", ptr);
+	printf("%d\n", *ptr);*/
+
+
+	//int* safer_ptr;		// = NULL
+	int* safer_ptr = NULL;	// 일반적인 사용법 -> 포인터 초기화
+	int a1 = 123;
+	int b;
+	scanf("%d", &b);
+
+	//safer_ptr = &a;		// 직접 할당
+
+	if (b % 2 == 0) {	// 더 많이 쓰임
+		safer_ptr = &a1;
+	}
+
+	if (safer_ptr != NULL) {
+		printf("%p\n", safer_ptr);
+	}
+
+	if (safer_ptr != NULL) {
+		printf("%d\n", *safer_ptr);
+	}
+
+	return 0;
+}
