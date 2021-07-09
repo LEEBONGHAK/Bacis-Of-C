9a0c3ca2e9c5000b001b01d4ff082331b0e75eb1
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-09 14:49:19 +0900

    9.13 포인터의 기본적인 사용법

---
+++ basic_usage_of_pointer/basic_usage_of_pointer.c
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WRNINGS
+
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	
+	a = 123;
+
+	int *a_ptr;		// * : asterisk
+
+	a_ptr = &a;		// & : address-of operator
+	printf("%d %d %p\n", a, *a_ptr, a_ptr);
+
+	*a_ptr = 456;	// * : re-direction or de-referencing
+	printf("%d %d %p\n", a, *a_ptr, a_ptr);
+
+
+	//b = a_ptr;
+	b = *a_ptr;
+
+	printf("%d\n", b);
+
+
+	*a_ptr = 789;
+
+	printf("%d %p\n", b, &b);
+	printf("%d %d %p\n", a, *a_ptr, a_ptr);
+
+
+	b = 12;
+	
+	printf("%d\n", b);
+	printf("%d %d %p\n", a, *a_ptr, a_ptr);
+
+	
+	a = 1004;
+
+	printf("%d %d %p\n", a, *a_ptr, a_ptr);
+
+
+	return 0;
+}
