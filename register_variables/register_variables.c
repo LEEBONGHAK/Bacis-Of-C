510547a8f53cc17263a04f49c6cf145a8abb2d6c
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-19 21:32:39 +0900

    12.6 레지스터 변수

---
+++ register_variables/register_variables.c
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+void temp(register int r) {
+	// do something with r
+}
+
+int main() {
+	register int r;		// 컴파일러에게 변수가 가급적 register에 위치하도록 요청하는 정도 - 따라서, 항상 register에 위치하는 것이 아님 
+	r = 123;
+
+	// register의 경우 주소 호출 불가
+	//printf("%p\n", r);
+	//int* ptr = &r;	
+
+	// 'register double'의 경우 컴파일러에 따라 컴파일러가 안될 수도 있음
+
+	return 0;
+}
