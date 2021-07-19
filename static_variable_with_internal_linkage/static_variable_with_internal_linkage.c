208d7d056759f7f88e02208a7fccd2b643e98f78
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-20 01:38:59 +0900

    12.9 정적 변수의 내부 연결 internal linkage

---
+++ static_variable_with_internal_linkage/static_variable_with_internal_linkage.c
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+/*
+	Static variable with internal linkage
+	- File scope, internal linkage, static storage duration
+	- Internal storage class
+*/
+
+int g_int = 123;				// defining declaration	(external)
+//static int g_int = 123;		// defining declaration (internal) - cannot use external linkage (linking error)
+
+void fun();
+void fun_second();
+void fun_third();
+
+int main() {
+	fun();
+	fun_second();
+	fun_third();
+	printf("\n");
+
+	return 0;
+}
+
+void fun() {
+	extern int g_int;		// Optional
+
+	g_int += 1;
+	printf("g_int in fun() %d %p\n", g_int, &g_int);
+}
