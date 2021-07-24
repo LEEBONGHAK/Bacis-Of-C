2a55abcfc879174114ba093f336b5572ddc42b8a
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-07-24 21:21:33 +0900

    14.19 열거형 연습문제

---
+++ enumerated_types_example/enumerated_types_example.c
@@ -0,0 +1,61 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>		// strcmp()
+#include <stdbool.h>
+
+enum spectrum { red, orange, yellow, green, blue };
+const char* colors[] = { "red", "orange", "yellow", "green", "blue" };
+
+#define LEN		30
+
+int main() {
+	char choice[LEN] = { 0, };
+	enum spectrum color;
+	bool color_is_found = false;
+
+	while (1) {
+		printf("Input a color name (empty line to quit)\n");
+		
+		if (scanf("%[^\n]%*c", choice) != 1) {
+			break;
+		}
+
+		for (color = red; color < blue; color++) {
+			if (strcmp(choice, colors[color]) == 0) {
+				color_is_found = true;
+				break;
+			}
+		}
+
+		if (color_is_found) {
+			switch (color)
+			{
+			case red:
+				printf("Red roses\n");
+				break;
+			case orange:
+				printf("Orange sunset\n");
+				break;
+			case yellow:
+				printf("Yellow sunflowers\n");
+				break;
+			case green:
+				printf("Green apples\n");
+				break;
+			case blue:
+				printf("Blue ocean\n");
+				break;
+			}
+		}
+		else {
+			printf("Please try different color %s.\n", choice);
+		}
+
+		color_is_found = false;
+	}
+
+	printf("Good bye!\n");
+
+	return 0;
+}
