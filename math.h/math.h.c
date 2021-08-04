64ba25e08302982f7a82ebf301124ee0feb15cd2
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-08-04 15:04:37 +0900

    16.13 표준 수학 라이브러리

---
+++ math.h/math.h.c
@@ -0,0 +1,61 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#include <math.h>
+//#include <tgmath.h>		// type generic macros
+
+// Generic Selection Expression - not allowed in VS
+/*
+#define SQRT(X) _Generic((X),\
+ long double: sqrl,\
+ float: sqrtf,\
+ default: sqrt,\
+)
+*/
+
+int main() {
+	printf("cos(PI) = %f\n\n", cos(3.141592));
+
+	/*
+		Pythagorean theorem
+		http://en.wikipedia.org/wiki/Pythagorean_theorem
+	*/
+	double c = 5.0, b = 4.0, a;		// a??
+	a = sqrt(c * c - b * b);
+	printf("a = %f\n", a);
+
+	float cf = 5.0f, bf = 4.0f, af;
+	af = sqrtf(cf * cf - bf * bf);
+	printf("a = %f\n\n", af);
+
+
+
+	/*	Not allowed in VS
+	
+	// Type Variants
+	double t1 = SQRT(2.0f);
+	double t2 = SQRT(2.0);
+
+	if (t1 == t2) {
+		printf("Identical\n");
+	}
+	else {
+		printf("Not identical\n");
+	}
+
+
+	// tgmath.h Library
+	double a1 = sqrt(2.0f);
+	double a2 = sqrt(2.0);
+
+	if (a1 == a2) {
+		printf("Identical\n");
+	}
+	else {
+		printf("Not identical\n");
+	}
+	*/
+
+	return 0;
+}
