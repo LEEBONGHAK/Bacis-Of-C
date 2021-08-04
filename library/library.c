38b710538572b02215a4840fdc827ab31a5465dd
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-08-04 14:33:52 +0900

    16.12 라이브러리

---
+++ library/library.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include "MyLibrary.h"
+
+#include <stdio.h>
+
+// library를 만들 때 main()가 없음
+
+/* 
+	library 설정 : project properties - configuration type - dynamic library 또는 static library
+
+	dynamic library : 프로그램이 실행이 될 때 그때 그때 동적으로 라이브러리를 가져와서 사용
+	static library : 프로그램에 라이브러리가 바로 포함이 되는 것
+
+	- debug, release 모드 둘 다 build 하는 것이 좋음
+*/
+
+void say_hello() {
+	printf("Hello\n");
+}
+
+void say_world() {
+	printf("World\n");
+}
---
+++ library_2/library_2.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+//#include "C:\Users\이봉학\Desktop\Basic-Of-C\library\MyLibrary.h"		// 절대 경로 사용법
+#include <MyLibrary.h>		// project properties - C/C++ - general - additional include directories 에 헤더 파일 전 절대 경로를 복붙하여 사용
+
+/*	
+	library linking error 해결법
+
+	1. project properties - Linker - general - additional library directories 에 
+	라이브러리가 들어있는 경로 추가
+	2. project properties - Linker - input - additional dependencies 에 
+	라이브러리 파일명 추가
+
+	- release 모드에서 build할 때는 설정을 잘 맞춰야함
+	- release/debug 모드에 따라 헤더 파일은 공통이지만 lib나 dll파일은 다르기 때문
+*/
+
+int main() {
+	say_hello();
+	say_world();
+
+	return 0;
+}
