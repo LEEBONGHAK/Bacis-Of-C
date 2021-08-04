76de9b4b26fb59ecfbfd325ec0ddcaafcaa332c4
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-08-04 15:41:45 +0900

    16.15 assert 라이브러리

---
+++ assert.h/assert.h.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#include <assert.h>		// assert library
+
+//#include <limits.h>
+//_Static_assert(CHAR_BIT == 16, "16-bit char falsely assumed");  // not allowed in VS - 컴파일 타임에서 잡아주는 assert
+
+int divide(int a, int b);
+
+int main() {
+	int a, b;
+	int f = scanf("%d%d", &a, &b);
+
+	printf("a / b = %d\n", divide(a, b));
+
+	return 0;
+}
+
+int divide(int a, int b) {
+	assert(b != 0);		// 반드시 만족해야하는 조건 - 디버깅 도구 중 하나
+
+	//if (b == 0) {
+	//	printf("Cannot divide\n");
+	//	exit(1);
+	//}
+
+	return a / b;
+}
